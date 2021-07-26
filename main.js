7674c79b0ea3126dd27fe99bc9b63825ecab224a
Faisal Ansari <84059960+MeFaisalAnsari@users.noreply.github.com>
2021-07-26 17:33:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,32 @@ $(document).ready(function () {
   $(".burger").click(function () {
     $(".header").toggleClass("burger-clicked");
   });
+
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("header").style.top = "0";
+      $(".header").css("background-color", "rgba(255, 255, 255, 0.3)");
+    } else {
+      document.getElementById("header").style.top = "-81px";
+    }
+    prevScrollpos = currentScrollPos;
+
+    if (window.pageYOffset == 0) {
+      $(".header").css("background", "none");
+    }
+  };
+
+  $(".burger").click(function () {
+    $("body").toggleClass("position-fixed");
+  });
+
+  $(".img-down").click(function () {
+    $("html, body").animate(
+      { scrollTop: $("#after-display").offset().top },
+      "slow"
+    );
+    return false;
+  });
 });
